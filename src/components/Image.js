6f621cfe3ee03ef59a230d540b26fc381c3b150b
CiprianaM <cipriana2001@gmail.com>
2020-04-20 17:23:28 +0200

    add isInCart state, cartIcon function

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types';
 import {Context} from "../Context"
 
 function Image({className, img}) {
-    const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context)
+    const [hovered, setHovered] = useState(false);
+    const [isInCart, setIsInCart] = useState(false);
+    const {toggleFavorite, addCartItem, removeCartItem} = useContext(Context);
+    //ri-shopping-cart-fill cart
+    function handleCartClick (img) {
+        if (isInCart) {
+            removeCartItem(img);
+
+        } else {
+            addCartItem(img);
+        }
+        setIsInCart(prev => !prev);
+    }
 
     function heartIcon() {
         if(img.isFavorite) {
@@ -12,19 +23,28 @@ function Image({className, img}) {
         } else if(hovered) {
             return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         }
+
+    }
+    function cartIcon() {
+        if(isInCart) {
+            return <i className="ri-add-circle-line cart" onClick={() => handleCartClick(img)}></i>
+        } else if(hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => handleCartClick(img)}></i>
+        }
     }
 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    // const cartIcon = hovered && <i className="ri-add-circle-line cart" onClick={() => addCartItem(img)}></i>
 
     return (
         <div
-            className={`${className} image-container`}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
+        className={`${className} image-container`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
         >
+        {/* <i class="ri-shopping-cart-fill"></i> */}
             <img src={img.url} className="image-grid"/>
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
 }
