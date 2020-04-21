131b43bf9f3e41921f607ab0654613b67fffd411
CiprianaM <cipriana2001@gmail.com>
2020-04-21 15:44:32 +0200

    add place order functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,20 +1,31 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import {Context} from "../Context"
 import CartItem from "../components/CartItem"
 
 function Cart() {
-    const {cartItems} = useContext(Context)
-    const cartItemElements = cartItems.map(item => (
-        <CartItem key={item.id} item={item} />
-    ))
+    const {cartItems, emptyCart} = useContext(Context)
+    const [textDisplayed, setTextDisplayed] = useState("Place Order");
+    let totalPrice = 0;
+    const cartItemElements = cartItems.map(item => {
+        totalPrice = totalPrice+5.99;
+        return <CartItem key={item.id} item={item} />
+    })
+    const handleOrder = () => {
+        setTextDisplayed("Ordering...");
+        setTimeout(() => {
+            emptyCart();
+            setTextDisplayed("Place Order");
+            console.log("Order placed!")
+        }, 3000)
+    }
 
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+    <p className="total-cost">Total: {totalPrice.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
             <div className="order-button">
-                <button>Place Order</button>
+    <button onClick={()=>handleOrder()}>{textDisplayed}</button>
             </div>
         </main>
     )
