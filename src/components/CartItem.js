934d17ebe688356169e4ace52d62af97e759d48c
CiprianaM <cipriana2001@gmail.com>
2020-04-21 16:30:55 +0200

    add cutom hook to CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,16 +1,16 @@
 import React, {useContext, useState} from "react";
 import {Context} from "../Context";
+import useHover from "../hooks/useHover";
 
 function CartItem({item}) {
   const {removeFromCart} = useContext(Context);
-  const [hovered, setHovered] = useState(false);
+  const [hovered, ref] = useHover();
   const binIcon = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
     return (
         <div className="cart-item">
             <i className={binIcon}
             onClick={()=> removeFromCart(item.id)}
-            onMouseEnter={()=> setHovered(true)}
-            onMouseLeave={()=> setHovered(false)}
+            ref={ref}
             >
             </i>
             <img src={item.url} width="130px" />
